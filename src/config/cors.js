aa137503aadd29fd78474f90fda91c8b18ec70e9
gulsherkooner <gulsher.kooner@gmail.com>
2025-05-12 15:55:42 +0530

    updated cors

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,21 +1,11 @@
 const cors = require('cors');
 
-module.exports = (req, res, next) => {
-  const allowedOrigins = ['https://api-gateway-eta-navy.vercel.app', 'http://localhost:3001','https://next-frontend-one-xi.vercel.app'];
-  const origin = req.headers.origin;
-
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Max-Age', '86400');
-
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
-
-  next();
+const corsOptions = {
+  origin: ['https://api-gateway-eta-navy.vercel.app', 'http://localhost:3001', 'https://next-frontend-one-xi.vercel.app'],
+  credentials: true, // Allow cookies (e.g., refreshToken)
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['X-Requested-With, Content-Type, Authorization'],
+  MAX_AGE: 86400,
 };
+
+module.exports = cors(corsOptions);
