57862fb5b36cd02ccc7c8aa2a2ef10fab66fd84a
gulsherkooner <gulsher.kooner@gmail.com>
2025-06-16 12:13:48 +0530

    updated reels page

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ const app = express();
 const PORT = process.env.PORT || 3004;
 
 // Increase payload size limit to handle large video uploads
-app.use(express.json({ limit: "100mb" }));
-app.use(express.urlencoded({ limit: "100mb", extended: true }));
-app.use(bodyParser.json({ limit: "100mb" }));
-app.use(bodyParser.urlencoded({ limit: "100mb", extended: true }));
+app.use(express.json({ limit: "500mb" }));
+app.use(express.urlencoded({ limit: "500mb", extended: true }));
+app.use(bodyParser.json({ limit: "500mb" }));
+app.use(bodyParser.urlencoded({ limit: "500mb", extended: true }));
 
 app.use(cors);
 app.use(express.json());
